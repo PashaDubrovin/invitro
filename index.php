ce7e0497150bf268608591dabc9b7534121307f3
PashaDubrovin <p.dubrovin@msk-uzi.ru>
2018-08-27 16:23:18 +0300

    WEB-1153
    Раздел "Центральная лаборатория"

---
+++ index.php
@@ -0,0 +1,242 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Центральная лаборатория");
+$APPLICATION->SetDirProperty("section_class", "content company-main");
+?>
+	<div class="service-home-row">
+		<div class="service-home-column-side">
+			<div style="display: block;" id="for_tab_1"> 
+      <p> Уже 12 лет Независимая лаборатория ИНВИТРО выступает в роли центральной лаборатории в рамках клинических исследований. В 2014 году мы приняли участие в клинических исследованиях, составляющих в среднем 8% от общего числа разрешенных исследований в России, по данному показателю наша компания уступает лишь европейским лабораториям. Всего за историю работы отдела мы приняли участие более, чем в 350 проектах. Ежегодно мы выполняем более 200 000 различных тестов в рамках клинических исследований. Существенным преимуществом является наличие четырех дополнительных лабораторных комплексов в России (Санкт-Петербург, Самара, Челябинск, Новосибирск), на базе которых также возможно проведение клинических исследований. Кроме этого, мы можем выступать в роли центральной лаборатории и в странах СНГ (Украина, Беларусь, Казахстан). </p>
+     
+      <p class="subhead">Оборудование</p>
+     
+      <p>Все лабораторные исследования проводятся с использованием высокотехнологичного оборудования, размещенного на территории московской лаборатории ИНВИТРО общей площадью 3500 м2. Выполнение лабораторных исследований на современных автоматических анализаторах позволяет обеспечить высокую воспроизводимость и качество результатов, а преаналитическая сортировочная станция OLA 2500 (Olympus Diagnostica, Германия) позволяет оптимизировать рабочие потоки и максимально сократить время, затрачиваемое от момента поступления биоматериала в лабораторию до получения результата. С перечнем используемого в лаборатории оборудования можно ознакомиться <a href="/about/equip/">здесь</a> </p>
+     
+      <p class="subhead">Возможности</p>
+     
+      <p>Лабораторный комплекс позволяет выполнять более 1500 видов лабораторных исследований, предназначенных для клинико-диагностических целей. Спектр исследований включает гематологические, биохимические, коагулологические, гормональные, иммунологические, микробиологические, цитологические, гистологические и цитогенетические тесты, определение маркеров воспалительного процесса и аутоиммунных заболеваний, ПЦР-диагностику и лекарственный мониторинг. Общая численность персонала лабораторного комплекса составляет 250 человек. Одновременно работают 120-150 квалифицированных специалистов. Лаборатория функционирует 7 дней в неделю, прием и регистрация биоматериала осуществляется круглосуточно.</p>
+     
+      <p>Специально для целей клинического исследования у нас существует возможность введения новых методик по выполнению нерутинных тестов, а также подключения внешних исполнителей, что позволяет значительно расширить панель предоставляемых нами тестов.</p>
+     
+      <p>В рамках клинических исследований взятие биоматериала может осуществляться на базе обширной сети медицинских офисов ИНВИТРО (более 700), что позволяет сократить вероятность преаналитических ошибок и значительно снижает стоимость курьерских услуг.</p>
+     
+      <p>В настоящий момент мы проводим последнюю фазу тестирования функции по предоставлению личного кабинета, и уже сейчас наши клиенты могут воспользоваться этим крайне полезным инструментом. Личный кабинет позволяет отслеживать результаты выполненных тестов практически в режиме реального времени. Кроме того, личный кабинет будет оснащен множеством других полезных функций, оптимизирующих проведение Вашего проекта.</p>
+     
+      <p class="subhead">Логистика </p>
+     
+      <p>Грамотно построенная логистика является одним из основных параметров своевременной доставки биоматериала в лабораторию для проведения исследования. Мы предоставляем логистические услуги с привлечением как собственной курьерской службы, так и сторонних служб, предлагая оптимальные варианты комбинаций по ценовому сегменту. Использование современных термоконтейнеров и хладоэлементов позволяет совершать перевозки с соблюдением температурного режима в диапазоне от -70°С до +37°С. Сроки доставки зависят от удаленности городов от лаборатории, так, доставка биоматериала в Москву из городов удаленностью до 2000 км осуществляется за 1 день, свыше 2000 км – за 2 дня.</p>
+     
+      <p class="subhead">Хранение</p>
+     
+      <p>В ходе выполнения клинического исследования зачастую возникает необходимость как непродолжительного, так и длительного хранения биологических образцов. С этой целью мы используем криохранилище для хранения биообразцов в жидком азоте (-195°С), морозильники (-20°С) и низкотемпературные морозильники (-70-96°С). Благодаря мощному дизельному генератору лаборатория может продолжать работу даже в случае полного отключения электроэнергии.</p>
+     
+      <p class="subhead">Стандарты качества </p>
+     
+      <p style="display: inline;">Анализаторы, используемые в лаборатории, объединены уникальной для России лабораторной информационной системой SafirLIS, специально адаптированной для ИНВИТРО шведской компанией PROFDOC LAB AB. SafirLIS обеспечивает надежную регистрацию, хранение и быстрый поиск результатов исследований. Лаборатория ИНВИТРО отвечает <a href="/about/licenses.php">стандартам</a> ГОСТ Р ИСО 15189-2015 (ISO 15189:2012) «Медицинские лаборатории. Особые требования к качеству и компетентности» и ГОСТ Р ИСО 9001-2015 (ISO 9001:2015) «Системы менеджмента качества. Требования». Мы регулярно участвуем в системах внешней оценки качества исследований: FSVOK (Россия), EQAS (США), RIQAS (Великобритания). Качество выполняемых анализов также подтверждено результатами </p>
+     
+      <div style="display: inline; position: relative;" id="accordion" class="ui-accordion ui-widget ui-helper-reset" role="tablist"><a href="audits.xlsx" class="ui-accordion-header ui-corner-top ui-accordion-header-collapsed ui-corner-all ui-state-default ui-accordion-icons" role="tab" id="ui-id-1" aria-controls="ui-id-2" aria-selected="false" aria-expanded="false" tabindex="0"><span class="ui-accordion-header-icon ui-icon ui-icon-triangle-1-e"></span>спонсорских аудитов</a>, 
+        <div class="inside_accord ui-accordion-content ui-corner-bottom ui-helper-reset ui-widget-content" id="ui-id-2" aria-labelledby="ui-id-1" role="tabpanel" aria-hidden="true" style="display: none;"> 
+          <table class="table_list"> 
+            <tbody> 
+              <tr><th>№</th><th>Компания-аудитор</th><th>Даты аудита</th></tr>
+             
+              <tr><td style="border-image: initial;">1</td><td style="border-image: initial;">Covance</td><td style="border-image: initial;">11.04.2005</td></tr>
+             
+              <tr><td style="border-image: initial;">2</td><td style="border-image: initial;">Viatris</td><td style="border-image: initial;">12.04.2005</td></tr>
+             
+              <tr><td style="border-image: initial;">3</td><td style="border-image: initial;">Covance</td><td style="border-image: initial;">09.06.2005</td></tr>
+             
+              <tr><td style="border-image: initial;">4</td><td style="border-image: initial;">Teva</td><td style="border-image: initial;">22.08.2005</td></tr>
+             
+              <tr><td style="border-image: initial;">5</td><td style="border-image: initial;">OCT</td><td style="border-image: initial;">22.09.2006</td></tr>
+             
+              <tr><td style="border-image: initial;">6</td><td style="border-image: initial;">BAS Medical</td><td style="border-image: initial;">01.11.2006</td></tr>
+             
+              <tr><td style="border-image: initial;">7</td><td style="border-image: initial;">Clinstar</td><td style="border-image: initial;">02.11.2006</td></tr>
+             
+              <tr><td style="border-image: initial;">8</td><td style="border-image: initial;">OCT</td><td style="border-image: initial;">08.12.2006</td></tr>
+             
+              <tr><td style="border-image: initial;">9</td><td style="border-image: initial;">PSI</td><td style="border-image: initial;">16.01.2007</td></tr>
+             
+              <tr><td style="border-image: initial;">10</td><td style="border-image: initial;">ICON Clinical Research Limited</td><td style="border-image: initial;">21.06.2007</td></tr>
+             
+              <tr><td style="border-image: initial;">11</td><td style="border-image: initial;">Novo Nordisk</td><td style="border-image: initial;">01.08.2007</td></tr>
+             
+              <tr><td style="border-image: initial;">12</td><td style="border-image: initial;">Novo Nordisk</td><td style="border-image: initial;">01.08.2007-02.08.2007</td></tr>
+             
+              <tr><td style="border-image: initial;">13</td><td style="border-image: initial;">Ascenta Therapeutics</td><td style="border-image: initial;">21.08.2007</td></tr>
+             
+              <tr><td style="border-image: initial;">14</td><td style="border-image: initial;">Isis Pharmaceuticals</td><td style="border-image: initial;">19.09.2007-20.09.2007</td></tr>
+             
+              <tr><td style="border-image: initial;">15</td><td style="border-image: initial;">Bayer</td><td style="border-image: initial;">09.10.2007</td></tr>
+             
+              <tr><td style="border-image: initial;">16</td><td style="border-image: initial;">Daiichi-Sankyo Pharma Development</td><td style="border-image: initial;">25.10.2007-26.10.2007</td></tr>
+             
+              <tr><td style="border-image: initial;">17</td><td style="border-image: initial;">Covance</td><td style="border-image: initial;">29.10.2007-30.10.2007</td></tr>
+             
+              <tr><td style="border-image: initial;">18</td><td style="border-image: initial;">PSI</td><td style="border-image: initial;">07.04.2008-08.04.2008</td></tr>
+             
+              <tr><td style="border-image: initial;">19</td><td style="border-image: initial;">Eurofins Medinet</td><td style="border-image: initial;">28.01.2009-29.01.2009</td></tr>
+             
+              <tr><td style="border-image: initial;">20</td><td style="border-image: initial;">PSI</td><td style="border-image: initial;">10.04.2009</td></tr>
+             
+              <tr><td style="border-image: initial;">21</td><td style="border-image: initial;">CSL Behring Distribution</td><td style="border-image: initial;">05.11.2009</td></tr>
+             
+              <tr><td style="border-image: initial;">22</td><td style="border-image: initial;">Biotest</td><td style="border-image: initial;">13.04.2010-14.04.2010</td></tr>
+             
+              <tr><td style="border-image: initial;">23</td><td style="border-image: initial;">Pharmanet Development Group</td><td style="border-image: initial;">29.04.2010</td></tr>
+             
+              <tr><td style="border-image: initial;">24</td><td style="border-image: initial;">PSI</td><td style="border-image: initial;">15.07.2010</td></tr>
+             
+              <tr><td style="border-image: initial;">25</td><td style="border-image: initial;">Clinstar</td><td style="border-image: initial;">22.10.2010</td></tr>
+             
+              <tr><td style="border-image: initial;">26</td><td style="border-image: initial;">Pharm-Olam International</td><td style="border-image: initial;">22.11.2010-23.11.2010</td></tr>
+             
+              <tr><td style="border-image: initial;">27</td><td style="border-image: initial;">Quintiles</td><td style="border-image: initial;">10.03.2011-11.03.2011</td></tr>
+             
+              <tr><td style="border-image: initial;">28</td><td style="border-image: initial;">PSI</td><td style="border-image: initial;">29.03.2011-30.03.2011</td></tr>
+             
+              <tr><td style="border-image: initial;">29</td><td style="border-image: initial;">PPDI</td><td style="border-image: initial;">05.04.2011</td></tr>
+             
+              <tr><td style="border-image: initial;">30</td><td style="border-image: initial;">Apitope Technology</td><td style="border-image: initial;">28.06.2011-30.06.2011</td></tr>
+             
+              <tr><td style="border-image: initial;">31</td><td style="border-image: initial;">Quest</td><td style="border-image: initial;">13.10.2011</td></tr>
+             
+              <tr><td style="border-image: initial;">32</td><td style="border-image: initial;">Covance</td><td style="border-image: initial;">31.10.2011-01.11.2011</td></tr>
+             
+              <tr><td style="border-image: initial;">33</td><td style="border-image: initial;">PSI</td><td style="border-image: initial;">19.01.2012</td></tr>
+             
+              <tr><td style="border-image: initial;">34</td><td style="border-image: initial;">Parexel</td><td style="border-image: initial;">25.01.2012-26.01.2012</td></tr>
+             
+              <tr><td style="border-image: initial;">35</td><td style="border-image: initial;">Forest Research Institute</td><td style="border-image: initial;">16.02.2012-17.02.2012</td></tr>
+             
+              <tr><td style="border-image: initial;">36</td><td style="border-image: initial;">Quintiles</td><td style="border-image: initial;">20.03.2012-21.03.2012</td></tr>
+             
+              <tr><td style="border-image: initial;">37</td><td style="border-image: initial;">Durata Therapeutics</td><td style="border-image: initial;">26.04.2012</td></tr>
+             
+              <tr><td style="border-image: initial;">38</td><td style="border-image: initial;">Chiltern</td><td style="border-image: initial;">16.05.2012-18.05.2012</td></tr>
+             
+              <tr><td style="border-image: initial;">39</td><td style="border-image: initial;">Nuvisan</td><td style="border-image: initial;">21.05.2012</td></tr>
+             
+              <tr><td style="border-image: initial;">40</td><td style="border-image: initial;">Cerexa</td><td style="border-image: initial;">09.08.2012-10.08.2012</td></tr>
+             
+              <tr><td style="border-image: initial;">41</td><td style="border-image: initial;">ICON Clinical Research Limited</td><td style="border-image: initial;">14.08.2012-16.08.2012</td></tr>
+             
+              <tr><td style="border-image: initial;">42</td><td style="border-image: initial;">iPharma</td><td style="border-image: initial;">18.04.2013, 07.05.2013</td></tr>
+             
+              <tr><td style="border-image: initial;">43</td><td style="border-image: initial;">MediVector</td><td style="border-image: initial;">31.05.2013</td></tr>
+             
+              <tr><td style="border-image: initial;">44</td><td style="border-image: initial;">MediVector</td><td style="border-image: initial;">24.07.2013</td></tr>
+             
+              <tr><td style="border-image: initial;">45</td><td style="border-image: initial;">PharmStandard</td><td style="border-image: initial;">11.09.2013</td></tr>
+             
+              <tr><td style="border-image: initial;">46</td><td style="border-image: initial;">Teva</td><td style="border-image: initial;">06.02.2014-07.02.2014</td></tr>
+             
+              <tr><td style="border-image: initial;">47</td><td style="border-image: initial;">InventiveHealth</td><td style="border-image: initial;">07.08.2014-08.08.2014</td></tr>
+             
+              <tr><td style="border-image: initial;">48</td><td style="border-image: initial;">Durata Therapeutics</td><td style="border-image: initial;">11.08.2014</td></tr>
+             
+              <tr><td style="border-image: initial;">49</td><td style="border-image: initial;">OCT</td><td style="border-image: initial;">10.09.2014</td></tr>
+             
+              <tr><td style="border-image: initial;">50</td><td style="border-image: initial;">PSI</td><td style="border-image: initial;">17.12.2014-18.12.2014</td></tr>
+             
+              <tr><td style="border-image: initial;">51</td><td style="border-image: initial;">Quintiles</td><td style="border-image: initial;">24.02.2015-25.02.2015v </td></tr>
+             
+              <tr><td style="border-image: initial;">52</td><td style="border-image: initial;">Ipsen</td><td style="border-image: initial;">21.07.2015-22.07.2015</td></tr>
+             </tbody>
+           </table>
+         </div>
+       </div>
+     которых за историю работы отдела «Клинические исследования» насчитывается более 50. 
+      <p></p>
+     
+      <p class="subhead">Команда</p>
+     
+      <p>Общее количество сотрудников отдела «Клинические исследования» насчитывает 27 человек, из них 14 – обеспечивают бесперебойное выполнение тестов и выдачу результатов. Все проекты распределены между 4 проектными менеджерами, хорошо владеющими знаниями английского языка, прошедшими полноценный тренинг по ICH GCP, имеющими опыт участия в конференциях (в том числе на английском языке), спонсорских аудитах.</p>
+     
+      <p class="subhead">Перечень дополнительных услуг</p>
+     
+      <ul> 
+        <li>Создание персонализированных лабораторных инструкций для исследователей, обучение правилам взятия биоматериала</li>
+       
+        <li>Печать направительных бланков, лабораторных инструкций</li>
+       
+        <li>Организация комплектации лабораторных наборов</li>
+       
+        <li>Логистические услуги по доставке биоматериала, лабораторных наборов и сопутствующей документации</li>
+       
+        <li>Выгрузка базы данных в требуемом Вами формате</li>
+       
+        <li>Выступление на исследовательском митинге с презентацией лабораторной части клинического исследования</li>
+       
+        <li>Подготовка биоматериала к отправке</li>
+       
+        <li>Хранение биоматериала с соблюдением требуемого температурного режима</li>
+       
+        <li>Утилизация биоматериала</li>
+       </ul>
+     
+      <p class="subhead">Кроме этого в каждом проекте Вам будут предоставлены</p>
+     
+      <ul> 
+        <li>Бесплатная подготовительная работа по изучению лабораторных процедур исследования и предоставление коммерческого предложения</li>
+       
+        <li>Персональный квалифицированный менеджер проекта</li>
+       
+        <li>Работа с исследовательскими центрами по уточнению информации на направительном бланке, своевременное решение неразрешенных вопросов</li>
+       
+        <li>Сопровождение аудитов, мониторинговых визитов в центральную лабораторию</li>
+       
+        <li>Предоставление таблиц референсных значений и методологий, CV руководителей лабораторий, сертификатов участия в системах внешнего контроля качества</li>
+       
+        <li>Предоставление результатов лабораторных тестов в удобном для Вас формате</li>
+       
+        <li>Отслеживание критических значений результатов анализов и своевременное уведомление исследовательских центров</li>
+       </ul>
+     
+      <p class="subhead">Контакты</p>
+     
+      <p> Отдел «Клинические исследования» ИНВИТРО 
+        <br>
+       Годунов Роман 
+        <br>
+       руководитель отдела «клинические исследования» 
+        <br>
+       E-mail: <a href="mailto:rgodunov@invitro.ru">rgodunov@invitro.ru</a>; <a href="mailto:trials@invitro.ru">trials@invitro.ru</a> 
+        <br>
+       
+        <br>
+       </p>
+     	 </div>
+		</div>
+	</div>
+
+	<script language="JavaScript">
+
+	  $(function() {
+    $( "#accordion, #accordionn" ).accordion({
+				header: "a",
+				active:false,
+				collapsible:true,
+				heightStyle: "content",
+				autoHeight:false
+	});
+ 
+    var lincks=$('.small_tabs').find('li');
+	lincks.removeClass('active');
+	lincks.each(function(){
+		var ind=($(this).index()+1);
+		if(ind==1){
+			$(this).addClass('active');
+			$('#for_tab_'+ind).css('display','block');
+		}
+		else{
+			$('#for_tab_'+ind).css('display','none');
+		}
+	});
+
+  
+  });
+</script>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
